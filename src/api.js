c2ce0701ab3caf6e11acce390bf8dbda4f9d0da0
Catalina Quidenao <c.a.p.quidenao@gmail.com>
2025-09-26 09:59:40 -0300

    camaras

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // ========= API base =========
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 export default API_URL;   // 👈 export default para usar en Camaras.jsx
 
 console.log("🚀 API_URL en runtime:", API_URL);
